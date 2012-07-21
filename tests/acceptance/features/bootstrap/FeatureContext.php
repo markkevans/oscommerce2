4bd7c09fe97adee0a0a097476857209e1126fb69
Mark Evans <markkevans@gmail.com>
2012-07-21 16:13:59 +0100

    Add Phabric support for testing data

--- tests/acceptance/features/bootstrap/FeatureContext.php
+++ tests/acceptance/features/bootstrap/FeatureContext.php
@@ -2,23 +2,50 @@
 
 use Behat\Behat\Context\ClosuredContextInterface,
     Behat\Behat\Context\TranslatedContextInterface,
-    Behat\Behat\Context\BehatContext,
     Behat\Behat\Exception\PendingException;
+
+use Behat\MinkExtension\Context\MinkContext;
+use Behat\Behat\Context\BehatContext,
+    Behat\Behat\Event\SuiteEvent,
+    Behat\Behat\Event\ScenarioEvent;
+
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
+use Phabric\Factory as pFactory;
+use Phabric\Phabric;
 
-//
-// Require 3rd-party libraries here:
-//
-//   require_once 'PHPUnit/Autoload.php';
-//   require_once 'PHPUnit/Framework/Assert/Functions.php';
-//
+require_once 'PHPUnit/Autoload.php';
+require_once 'PHPUnit/Framework/Assert/Functions.php';
+
+$phaLoader = new \Doctrine\Common\ClassLoader('Phabric', realpath(__DIR__ . '/../../../lib/'));
+$phaLoader->register();
+
+$docLoader = new \Doctrine\Common\ClassLoader('Doctrine\DBAL', __DIR__ . '/../../../lib/Vendor/Doctrine/lib');
+$docLoader->register();
+
+$docComLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', __DIR__ . '/../../../lib/Vendor/Doctrine/lib/vendor/doctrine-common/lib');
+$docComLoader->register();
 
 /**
  * Features context.
  */
-class FeatureContext extends BehatContext
+class FeatureContext extends MinkContext
 {
+	
+    /**
+     * The Phabric Bus
+     *
+     * @var Phabric\Bus
+     */
+    private $phabric;
+
+    /**
+     * The Databse Connection.
+     *
+     * @var Doctrine\DBAL\Connection
+     */
+    private static $db;
+
     /**
      * Initializes context.
      * Every scenario gets it's own context object.
@@ -27,20 +54,69 @@ class FeatureContext extends BehatContext
      */
     public function __construct(array $parameters)
     {
-        // Initialize your context here
+        $config = new \Doctrine\DBAL\Configuration();
+
+        self::$db = \Doctrine\DBAL\DriverManager::getConnection(array(
+                    'dbname' => $parameters['database']['dbname'],
+                    'user' => $parameters['database']['username'],
+                    'password' => $parameters['database']['password'],
+                    'host' => $parameters['database']['host'],
+                    'driver' => $parameters['database']['driver'],
+                ));
+ 
+        $datasource = new \Phabric\Datasource\Doctrine(self::$db, $parameters['Phabric']['entities']);
+        $this->phabric = new Phabric($datasource);
+        $this->phabric->createEntitiesFromConfig($parameters['Phabric']['entities']);		
     }
 
 	/**
-     * @Given /^I select the "([^"]*)" radio button$/
+	 * @BeforeSuite
+	 */
+	public static function prepare(SuiteEvent $event)
+	{
+       $sql = file_get_contents(__DIR__ . '/../../config/fixtures/reset.sql');
+        self::$db->query($sql);
+	}
+
+	/**
+     * @Given /^The following customers exist$/
      */
-    public function iSelectTheRadioButton($radio_label) {
-      $radio_button = $this->getSession()->getPage()->findField($radio_label);
-      if (null === $radio_button) {
-        throw new ElementNotFoundException(
-          $this->getSession(), 'form field', 'id|name|label|value', $field
-        );
-      }
-      $value = $radio_button->getAttribute('value');
-      $this->fillField($radio_label, $value);
+    public function theFollowingCustomersExist(TableNode $table) {
+        $this->phabric->insertFromTable('customers', $table);
+    }
+	
+	/**
+     * @When /^I select all records from the customers table$/
+     */
+    public function iSelectAllRecordsFromTheCustomersTable() {
+        $sql = 'SELECT * FROM customers';
+
+        $rows = self::$db->fetchAll($sql);
+        
+        $this->qResult = $rows;
+    }
+	
+	/**
+     * @Then /^I should see the following records$/
+     */
+    public function iShouldSeeTheFollowingRecords(TableNode $table) {
+        
+        // Get the col names
+        $topRow = reset($this->qResult);
+
+        // Col names - id
+        $cols = array_keys($topRow);
+        array_shift($cols);
+
+        $actualResults = array($cols);
+
+        foreach ($this->qResult as $row) {
+            // Remove the id from the results
+            array_shift($row);
+            $actualResults[] = array_values($row);
+        }
+
+        $expectedResults = $table->getRows();
+        assertEquals($expectedResults, $actualResults);
     }
 }
--- tests/acceptance/features/catalog/account.feature
+++ tests/acceptance/features/catalog/account.feature
@@ -8,6 +8,15 @@ Feature: Account
     When I follow "create an account"
     Then I should see "My Account Information"
 
+  Scenario: Test Customer Creation
+    Given The following customers exist
+    | Email  | Gender | Firstname | Lastname | DOB | Phone | Password | Fax | Newsletter | Address |
+    | testing2@example.com | m | Behat | User | 1970-05-21 | 01204 2324224 | $P$DTl3oE1hOday7GR40bSpLEjGimHVJI1 | 01331 5335153 | Y | 1 |
+    When I select all records from the customers table
+    Then I should see the following records
+    | customers_gender | customers_firstname  | customers_lastname | customers_dob | customers_email_address | customers_default_address_id | customers_telephone | customers_fax | customers_password | customers_newsletter |
+    | m | Behat | User | 1970-05-21 00:00:00 | testing2@example.com | 1 | 01204 2324224 | 01331 5335153 | $P$DTl3oE1hOday7GR40bSpLEjGimHVJI1 | Y |
+
   Scenario: Create an Account
     Given I am on "/"
     When I follow "create an account"
@@ -15,7 +24,7 @@ Feature: Account
     And I fill in "firstname" with "Behat"
     And I fill in "lastname" with "Behat"
     And I fill in "dob" with "05/21/1970"
-    And I fill in "email_address" with "testing@osservices.co.uk"
+    And I fill in "email_address" with "testing@example.com"
     And I fill in "street_address" with "123 Fake Street"
     And I fill in "postcode" with "BH1 5RT"
     And I fill in "city" with "Fake City"
@@ -28,9 +37,12 @@ Feature: Account
     Then I should see "Your Account Has Been Created!"
 
   Scenario: Login to Account
-    Given I am on "/"
+    Given The following customers exist
+    | Email  | Gender | Firstname | Lastname | DOB | Phone | Password | Fax | Newsletter | Address |
+    | testing2@example.com | m | Behat | User | 1970-05-21 | 01204 2324224 | $P$DTl3oE1hOday7GR40bSpLEjGimHVJI1 | 01331 5335153 | Y | 1 |
+    And I am on "/"
     When I follow "log yourself in"
-    And I fill in "email_address" with "testing@osservices.co.uk"
+    And I fill in "email_address" with "testing2@example.com"
     And I fill in "password" with "password"
     And I press "tdb5"
     Then I should see "Welcome back Behat!"
