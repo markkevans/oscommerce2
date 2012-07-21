f855c7e6feff4b67d330336ff7a434b3ea357da0
Mark Evans <markkevans@gmail.com>
2012-07-21 16:12:43 +0100

    Add Phabric support for testing data

---
+++ tests/acceptance/config/fixtures/reset.sql
@@ -0,0 +1,15 @@
+TRUNCATE TABLE customers;
+TRUNCATE TABLE customers_basket;
+TRUNCATE TABLE customers_basket_attributes;
+TRUNCATE TABLE customers_info;
+
+TRUNCATE TABLE address_book;
+
+TRUNCATE TABLE orders;
+TRUNCATE TABLE orders_products;
+TRUNCATE TABLE orders_products_attributes;
+TRUNCATE TABLE orders_products_download;
+TRUNCATE TABLE orders_status_history;
+TRUNCATE TABLE orders_total;
+
+TRUNCATE TABLE whos_online;
